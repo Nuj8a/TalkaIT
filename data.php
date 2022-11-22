9288f772a8e4a294aec081aa4f224a8d173bd2ab
Nuj8a <anujrajamhrz@gmail.com>
2022-11-22 20:02:25 +0545

    commit

--- data.php
+++ data.php
@@ -1,4 +1,7 @@
 <?php
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $_signUpEmail = $_POST['email'];
+    $_signUpPass = $_POST['password'];
 
 $servername = "localhost";
 $username = "root";
@@ -12,8 +15,18 @@ $conn = mysqli_connect($servername,$username,$password,$database);
 //checkin connection
 if(!$conn){
     die("Connection Failed : ". mysqli_connect_error());
-}else{
-    echo "connection successful<br>";
+}
+else{
+    $sql = "INSERT INTO `signup`(`Email`,`password`) VALUES ('$_signUpEmail','$_signUpPass')";
+    $result = mysqli_query($conn,$sql);
+    
+    if($result){
+        echo "The record has been set";
+    }
+    else{
+        echo "the records couldn't be created--->". mysqli_error($conn);
+    }
+}
 }
 
 // //creating db
@@ -27,7 +40,14 @@ if(!$conn){
 //     echo "The db was not created successfully because of this error --->". mysqli_error($conn);
 // }
 
-$sql = "CREATE TABLE `SignUp` (`Email` VARCHAR(25) NOT NULL , `password` VARCHAR(16) NOT NULL)";
+// $sql = "CREATE TABLE `SignUp` (`Email` VARCHAR(25) NOT NULL , `password` VARCHAR(16) NOT NULL)";
+// $result = mysqli_query($conn,$sql);
+// if($result){
+//     echo "the table was created <br>";
+// }else{
+//     echo "the table couldn't be created--->". mysqli_error($conn);
+// }
+
 
 
 
